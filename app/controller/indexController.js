2058ca15661073678a0068968b2d2ce0e28646a6
chaojijiao <2332375762@qq.com>
2017-12-04 10:24:51 +0800

    frist

---
+++ app/controller/indexController.js
@@ -0,0 +1,12 @@
+app.controller("indexController",function($scope){
+    $scope.title = "公司";
+    $scope.info = [
+        {
+            'img':"app/content/img/2.png",
+            'title':"百度",
+            'add':"北京市海淀区西北旺",
+            'list':'互联网|已上市|10000人以上',
+            'zhaopin':"前端工程师等2382个职位"
+        }
+    ]
+})
